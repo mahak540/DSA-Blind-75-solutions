8ccd6e34a8c4f47c1090aba7a9c918fcb03aedd8
mahak540 <162689029+mahak540@users.noreply.github.com>
2025-07-15 10:19:09 +0530

    Add Missing Number solution using sum difference in Binary folder
    Missing Number solution in C++ using sum difference
    
    Start with res = n.
    
    For each index i, add i and subtract nums[i] from res.
    
    At the end, res equals the missing number because:
    (0+1+...+n) - sum(nums) = missing number.
    
    Time Complexity: O(n)
    Space Complexity: O(1)

---
+++ Binary/missingnumber.cpp
@@ -0,0 +1,9 @@
+ int missingNumber(vector<int>& nums) {
+        int res = nums.size();
+        
+        for (int i = 0; i < nums.size(); i++) {
+            res += i - nums[i];
+        }
+        
+        return res; 
+    }
