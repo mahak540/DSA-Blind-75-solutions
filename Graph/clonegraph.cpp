f0d94a6477abcafcca0dd5da34ae18303db121b6
mahak540 <162689029+mahak540@users.noreply.github.com>
2025-08-05 12:00:41 +0530

    Update clonegraph.cpp
    [Approach] Using BFS traversal - O(V+E) Time and O(V) Space
    In the BFS approach, the graph is cloned iteratively using a queue. We begin by cloning the initial node and placing it in the queue. As we process each node from the queue, we visit its neighbors. If a neighbor has not been cloned yet, we create a clone, store it in a map, and enqueue it for later processing. We then add the clone of the neighbor to the current node’s clone’s list of neighbors. This process continues level by level, ensuring that all nodes are visited in breadth-first order. BFS is particularly useful for avoiding deep recursion and handling large or wide graphs efficiently.

--- Graph/clonegraph.cpp
+++ Graph/clonegraph.cpp
@@ -1,22 +1,31 @@
 Node* cloneGraph(Node* node) {
-        map<Node*, Node*> oldToNew;
-        return dfs(node, oldToNew);
-    }
-    Node* dfs(Node* node, map<Node*, Node*>& oldToNew) {
-        if (node == nullptr) {
-            return nullptr;
+        if(!node)
+        return NULL;
+        map<Node*,Node*>mp;
+        queue<Node*>q;
+        Node* clone=new Node();
+        
+        clone->val=node->val;
+        mp[node]=clone;
+        q.push(node);
+        
+        while(!q.empty())
+        {
+            Node* u=q.front();
+            q.pop();
+            
+            for(auto& neighbor:u->neighbors)
+            {
+                if(mp.find(neighbor)==mp.end())
+                {
+                    Node* neighborClone=new Node();
+                    neighborClone->val=neighbor->val;
+                    mp[neighbor]=neighborClone;
+                    q.push(neighbor);
+                }
+                mp[u]->neighbors.push_back(mp[neighbor]);
+            }
         }
+        return mp[node];
 
-        if (oldToNew.count(node)) {
-            return oldToNew[node];
-        }
-
-        Node* copy = new Node(node->val);
-        oldToNew[node] = copy;
-
-        for (Node* nei : node->neighbors) {
-            copy->neighbors.push_back(dfs(nei, oldToNew));
-        }
-
-        return copy;
     }
